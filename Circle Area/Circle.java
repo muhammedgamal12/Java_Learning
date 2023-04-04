1a6435c9f29e3f9fd81c0e2930c50f72b4f86f29
muhamed gamal <m.gamal2288@gmail.com>
2023-04-04 22:46:33 +0200

    week 1 tasks

---
+++ Circle Area/Circle.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double r = 2.00;
+  final double n = 3.14159;
+        double A = n*(r*r);
+
+        System.out.println("A="+String.format("%.4f", A));
+    }
+}
